4f30809ec70f1923dfd6dbf55d8a05694b3144fb
rishikabharti <bhartirishika@gmail.com>
2021-03-24 19:25:13 -0400

    added entries in schema.sql

--- sqlite/schema.sql
+++ sqlite/schema.sql
@@ -1,1 +1,5 @@
 create table books(id INTEGER PRIMARY KEY, name TEXT, topic TEXT, cost INTEGER, stock INTEGER);
+insert into books(name, topic, cost, stock) values("How to get a good grade in 677 in 20 minutes a day", "Distributed Systems", 200, 200);
+insert into books(name, topic, cost, stock) values("RPCs for Dummies", "Distributed Systems", 200, 200);
+insert into books(name, topic, cost, stock) values("Xen and the Art of Surviving Graduate School", "Graduate School", 200, 200);
+insert into books(name, topic, cost, stock) values("Cooking for the Impatient Graduate Student", "Graduate School", 200, 200);
