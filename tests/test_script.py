ab201bdd385ad27837b8d4afe8007f31bb394b51
Noel Varghese <noel.varghese.22@gmail.com>
2021-03-29 02:21:22 -0400

    WIP: Fixed initial issues with script

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,20 +1,21 @@
 import requests
 
-def test_search_endpoint(input, output, test_case_num):
+print("Starting Testing Script")
 
+def test_search_endpoint(test_input, test_output, test_case_num):
    try:
-      response = requests.get('http://localhost:8000/search?topic=input')
-      if response == output:
-          print("Test case " + test_case_num + " passed")
+      response = requests.get('http://localhost:8000/search?topic=' + str(test_input))
+      if response.text == str(test_output):
+          print("Test case " + str(test_case_num) + " ....... Passed")
           return
    except ConnectionError as err:
       print(err)
-      print("Test case " + test_case_num + " failed")
+   print("Test case: " +str(test_case_num) + " ....... Failed")
    return
 
 
 test_search_endpoint('Graduate School', [{'id': 3, 'name': 'Xen and the Art of Surviving Graduate School'}, {'id': 4, 'name': 'Cooking for the Impatient Graduate Student'}], 1) 
-test_search_endpoint('Disributed Systems', [{'id': 3, 'name': 'Xen and the Art of Surviving Graduate School'}, {'id': 4, 'name': 'Cooking for the Impatient Graduate Student'}], 2)  
+#test_search_endpoint('Disributed Systems', [{'id': 3, 'name': 'Xen and the Art of Surviving Graduate School'}, {'id': 4, 'name': 'Cooking for the Impatient Graduate Student'}], 2)  
     
 def test_lookup_endpoint(input, output, test_case_num):
 
@@ -30,8 +31,8 @@ def test_lookup_endpoint(input, output, test_case_num):
 
 
 
-test_lookup_endpoint('4', [{'cost': 200, 'id': 4, 'name': 'Cooking for the Impatient Graduate Student', 'stock': 200, 'topic': 'Graduate School'}], 3)
-test_lookup_endpoint('1', [{'cost': 200, 'id': 4, 'name': 'Cooking for the Impatient Graduate Student', 'stock': 200, 'topic': 'Graduate School'}] , 4)
+#test_lookup_endpoint('4', [{'cost': 200, 'id': 4, 'name': 'Cooking for the Impatient Graduate Student', 'stock': 200, 'topic': 'Graduate School'}], 3)
+#test_lookup_endpoint('1', [{'cost': 200, 'id': 4, 'name': 'Cooking for the Impatient Graduate Student', 'stock': 200, 'topic': 'Graduate School'}] , 4)
 
 
 
@@ -50,4 +51,4 @@ def test_buy_endpoint(input, output, test_case_num):
    return
 
 
-test_buy_endpoint
+#test_buy_endpoint
